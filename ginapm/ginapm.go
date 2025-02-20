dfb259844c58c2f5db273b43c984f7dab38b0727
Emmanuel Emonueje Ebenezer <eebenezer949@gmail.com>
2025-02-20 10:02:50 +0100

    fix: Update ginapm module to use versioning

--- ginapm/ginapm.go
+++ ginapm/ginapm.go
@@ -9,11 +9,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Config holds required fields to accurately your error logs to telex
 type Config struct {
+	// TelexChanID - the id of the telex channel where logs pop up
 	TelexChanID string `json:"telex_channel_id"`
+	// ApmServerUrl - the url of the apm server sends logs to telex at intervals
+	// currently: "ttps://ekefan-go.onrender.com/error-log"
+	ApmServerUrl string `json:"apm_server_url"`
 }
-
-const serverURL = "https://ekefan-go.onrender.com/error-log"
 
 // GinAPM creates middleware to capture errors and send them to the APM server
 func GinAPM(config Config) gin.HandlerFunc {
@@ -42,7 +45,7 @@ func sendErrors(ctx *gin.Context, config Config) {
 	}
 
 	// Send errors to APM server
-	resp, err := http.Post(serverURL, "application/json", bytes.NewBuffer(jsonData))
+	resp, err := http.Post(config.ApmServerUrl, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		fmt.Println("Failed to send error log:", err)
 		return
